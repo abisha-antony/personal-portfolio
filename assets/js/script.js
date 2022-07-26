a63dd38fe3dc975edf2fc3828b8ba7b204e5bcc8
abisha-antony <abishaantony.v@gmail.com>
2022-07-26 12:33:15 +0530

    portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function expand_sidebar() {
+    document.getElementById('st1').style.display = "block";
+    document.getElementById('st2').style.display = "block";
+    document.getElementById('st3').style.display = "block";
+    document.getElementById('st4').style.display = "block";
+}
+function collapse_sidebar() {
+    document.getElementById('st1').style.display = "none";
+    document.getElementById('st2').style.display = "none";
+    document.getElementById('st3').style.display = "none";
+    document.getElementById('st4').style.display = "none";
+}
